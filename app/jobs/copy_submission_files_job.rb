2b0487c0c585b168449ece3e32f0488935c9003c
Keita Urashima <ursm@ursm.jp>
2021-12-30 16:38:32 +0900

    Write out the submission files atomically, and delete the blobs when finished

--- app/jobs/copy_submission_files_job.rb
+++ app/jobs/copy_submission_files_job.rb
@@ -2,15 +2,23 @@ class CopySubmissionFilesJob < ApplicationJob
   queue_as :default
 
   def perform(submission)
-    dest = Pathname.new(ENV.fetch('SUBMISSIONS_DIR')).join(submission.mass_id, submission.created_at.strftime('%Y%m%d'))
-    dest.mkpath
+    output_dir = Pathname.new(ENV.fetch('SUBMISSIONS_DIR'))
+    timestamp  = submission.created_at.strftime('%Y%m%d')
+    work       = output_dir.join('.work', submission.mass_id, timestamp)
+    dest       = output_dir.join(submission.mass_id)
+
+    work.mkpath
 
     submission.files.each do |attachment|
-      dest.join(attachment.filename.to_s).open 'wb' do |f|
+      work.join(attachment.filename.to_s).open 'wb' do |f|
         attachment.download do |chunk|
           f.write chunk
         end
       end
     end
+
+    FileUtils.move work, dest.tap(&:mkpath)
+
+    submission.files.purge
   end
 end
