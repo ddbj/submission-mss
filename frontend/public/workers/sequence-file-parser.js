ea2fa6fbfc05bcdd3fbdf02faf58be95cabe0f0b
Keita Urashima <ursm@ursm.jp>
2022-02-05 20:38:21 +0900

    Optimize

--- frontend/public/workers/sequence-file-parser.js
+++ frontend/public/workers/sequence-file-parser.js
@@ -1,43 +1,27 @@
 addEventListener('message', async (e) => {
   const [id, file] = e.data;
-  const reader     = file.stream().pipeThrough(new TextDecoderStream()).pipeThrough(new LineStream()).getReader();
+  const reader     = file.stream().getReader();
 
-  let done, value;
+  let done, bytes;
   let entriesCount = 0;
+  let isBOL        = true;
 
-  while (({done, value} = await reader.read()), !done) {
-    if (value.startsWith('>')) {
-      entriesCount++;
+  while (({done, value: bytes} = await reader.read()), !done) {
+    for (const byte of bytes) {
+      if (isBOL && byte === gt) {
+        entriesCount++;
+        isBOL = false;
+      } else if (byte === lf || byte === cr) {
+        isBOL = true;
+      } else {
+        isBOL = false;
+      }
     }
   }
 
   postMessage([null, [id, {entriesCount}]]);
 });
 
-class LineStream extends TransformStream {
-  constructor() {
-    super({
-      start() {
-        this.pending = '';
-      },
-
-      transform(chunk, controller) {
-        if (!chunk) { return; }
-
-        const buffer = this.pending + chunk;
-
-        this.pending = buffer.replaceAll(/[^\n\r]*(?:\r\n|\n|\r)/g, (line) => {
-          controller.enqueue(line);
-
-          return '';
-        });
-      },
-
-      flush(controller) {
-        if (this.pending === '') { return; }
-
-        controller.enqueue(this.pending);
-      }
-    });
-  }
-}
+const lf = '\n'.codePointAt(0);
+const cr = '\r'.codePointAt(0);
+const gt = '>'.codePointAt(0);
