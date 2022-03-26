e8085051f0c42824a3d963329729e1fd605665f7
Ryunosuke Sato <tricknotes.rs@gmail.com>
2022-03-27 00:10:30 +0900

    Handle error on direct upload

--- frontend/app/components/upload-progress-modal.js
+++ frontend/app/components/upload-progress-modal.js
@@ -3,6 +3,8 @@ import { action } from '@ember/object';
 import { service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 
+import { UploadError } from 'mssform/models/direct-upload';
+
 import Modal from 'bootstrap/js/src/modal';
 
 import UploadFiles from 'mssform/models/upload-files';
@@ -11,6 +13,8 @@ export default class UploadProgressModalComponent extends Component {
   @service session;
 
   @tracked uploadFiles;
+
+  @service session;
 
   @action setModal(element) {
     this.modal = new Modal(element);
@@ -26,8 +30,28 @@ export default class UploadProgressModalComponent extends Component {
     this.uploadFiles = new UploadFiles(files);
 
     this.modal.show();
-    const blobs = await this.uploadFiles.perform(this.session);
-    this.modal.hide();
+
+    let blobs;
+
+    try {
+      blobs = await this.uploadFiles.perform(this.session);
+    } catch(e) {
+      if (e instanceof UploadError)  {
+        if (e.status === 401) {
+          alert('Your session has been expired. Please re-login.');
+
+          this.session.invalidate();
+        } else if (e.status >= 500) {
+          alert('Upload failed for some reason. Please retry in a few minutes.');
+
+          return;
+        }
+      }
+
+      throw e;
+    } finally {
+      this.modal.hide();
+    }
 
     return blobs;
   }
