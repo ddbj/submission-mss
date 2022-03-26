2df090b207542ca6974820b622c8516cffef355b
Ryunosuke Sato <tricknotes.rs@gmail.com>
2022-03-26 23:56:19 +0900

    Throw error on upload failed

--- frontend/app/models/direct-upload.js
+++ frontend/app/models/direct-upload.js
@@ -13,14 +13,14 @@ export class DirectUpload {
   }
 
   async create(callback) {
-    const blob = new BlobRecord(this.file, await this.checksum, this.url)
+    const blob = new BlobRecordWithError(this.file, await this.checksum, this.url)
     notify(this.delegate, "directUploadWillCreateBlobWithXHR", blob.xhr)
 
     blob.create(error => {
       if (error) {
         callback(error)
       } else {
-        const upload = new BlobUpload(blob)
+        const upload = new BlobUploadWithError(blob)
         notify(this.delegate, "directUploadWillStoreFileWithXHR", upload.xhr)
         upload.create(error => {
           if (error) {
@@ -37,5 +37,29 @@ export class DirectUpload {
 function notify(object, methodName, ...messages) {
   if (object && typeof object[methodName] == "function") {
     return object[methodName](...messages)
+  }
+}
+
+export class UploadError extends Error {
+  constructor(_, target) {
+    super(...arguments)
+
+    this.target = target
+  }
+
+  get status() {
+    return this.target.xhr.status
+  }
+}
+
+class BlobRecordWithError extends BlobRecord  {
+  requestDidError() {
+    this.callback(new UploadError(`Error creating Blob for "${this.file.name}". Status: ${this.xhr.status}`, this))
+  }
+}
+
+class BlobUploadWithError extends BlobUpload {
+  requestDidError() {
+    this.callback(new UploadError(`Error storing "${this.file.name}". Status: ${this.xhr.status}`, this))
   }
 }
