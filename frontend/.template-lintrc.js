b3934397878ca7f19ce998152635d3f032160683
Keita Urashima <ursm@ursm.jp>
2023-08-30 08:09:02 +0900

    Lint

--- frontend/.template-lintrc.js
+++ frontend/.template-lintrc.js
@@ -4,7 +4,7 @@ module.exports = {
   extends: 'recommended',
 
   rules: {
-    'no-bare-strings': true,
+    // 'no-bare-strings': true,
     'no-curly-component-invocation': {allow: ['user-mass-dir']},
     'no-implicit-this': {allow: ['user-mass-dir']},
     'no-inline-styles': 'off',
--- frontend/app/models/upload.js
+++ frontend/app/models/upload.js
@@ -1,4 +1,4 @@
-import Model, { attr, belongsTo, hasMany } from '@ember-data/model';
+import Model, { attr, belongsTo } from '@ember-data/model';
 
 export default class Upload extends Model {
   @belongsTo submission;
