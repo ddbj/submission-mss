287a230d43eb9b827f28547619c64422ea6ab8fa
Keita Urashima <ursm@ursm.jp>
2023-11-26 22:10:35 +0900

    Fix Active Storage settings

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  # config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -65,4 +65,6 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  config.active_storage.service = :minio
 end
