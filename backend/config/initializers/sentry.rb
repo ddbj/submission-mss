15511b162365c0b86dde71be382454b00f9d5042
Keita Urashima <ursm@ursm.jp>
2024-02-19 08:52:37 +0900

    Set Sentry environment via SENTRY_CURRENT_ENV variable

--- backend/config/initializers/sentry.rb
+++ backend/config/initializers/sentry.rb
@@ -1,7 +1,6 @@
 if dsn = ENV['SENTRY_DSN']
   Sentry.init do |config|
     config.dsn                = dsn
-    config.environment        = ENV['SENTRY_ENVIRONMENT']
     config.breadcrumbs_logger = [:active_support_logger, :http_logger]
   end
 end
