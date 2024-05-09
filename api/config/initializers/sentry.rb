c35ba517604ba2b392f57ae478571be2d47c0a0f
Keita Urashima <ursm@ursm.jp>
2024-05-09 18:35:33 +0900

    Simplify Sentry config

--- api/config/initializers/sentry.rb
+++ api/config/initializers/sentry.rb
@@ -1,6 +1,3 @@
-if dsn = ENV['SENTRY_DSN']
-  Sentry.init do |config|
-    config.dsn                = dsn
-    config.breadcrumbs_logger = [:active_support_logger, :http_logger]
-  end
+Sentry.init do |config|
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 end
