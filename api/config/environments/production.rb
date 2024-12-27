a2346c3dd15d94eb8854d91fb03f284ce8fa7985
Keita Urashima <ursm@ursm.jp>
2024-12-27 20:38:35 +0900

    Revert "openssl_verify_mode: none"
    This reverts commit 2ac2fde91d2b69191d782a0f5d62692f9520cfde.

--- api/config/environments/production.rb
+++ api/config/environments/production.rb
@@ -94,12 +94,11 @@ Rails.application.configure do
   config.solid_queue.connects_to       = { database: { writing: :queue } }
 
   config.action_mailer.smtp_settings = {
-    address:             ENV.fetch("SMTP_ADDRESS"),
-    port:                ENV.fetch("SMTP_PORT"),
-    domain:              ENV.fetch("SMTP_DOMAIN"),
-    user_name:           ENV.fetch("SMTP_USERNAME"),
-    password:            ENV.fetch("SMTP_PASSWORD"),
-    authentication:      ENV.fetch("SMTP_AUTHENTICATION"),
-    openssl_verify_mode: "none"
+    address:        ENV.fetch("SMTP_ADDRESS"),
+    port:           ENV.fetch("SMTP_PORT"),
+    domain:         ENV.fetch("SMTP_DOMAIN"),
+    user_name:      ENV.fetch("SMTP_USERNAME"),
+    password:       ENV.fetch("SMTP_PASSWORD"),
+    authentication: ENV.fetch("SMTP_AUTHENTICATION")
   }.compact_blank
 end
