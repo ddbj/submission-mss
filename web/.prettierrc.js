92269ef3a410715224b61b34ea317f7d16c1c379
maimux2x <m0smile10often@gmail.com>
2025-02-25 11:59:43 +0900

    Update ember-cli

--- web/.prettierrc.js
+++ web/.prettierrc.js
@@ -3,12 +3,18 @@
 module.exports = {
   printWidth: 120,
   plugins: ['prettier-plugin-ember-template-tag'],
-
   overrides: [
     {
-      files: '*.{js,ts,gjs,gts}',
+      files: '*.{js,gjs,ts,gts,mjs,mts,cjs,cts}',
       options: {
         singleQuote: true,
+      },
+    },
+    {
+      files: '*.{gjs,gts}',
+      options: {
+        singleQuote: true,
+        templateSingleQuote: false,
       },
     },
   ],
