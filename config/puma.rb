6c13f545839399115a1aa02f5cf78cbdb49eaba1
Keita Urashima <ursm@ursm.jp>
2026-05-19 15:39:41 +0900

    Allow bind to UNIX socket

--- config/puma.rb
+++ config/puma.rb
@@ -29,7 +29,7 @@ threads_count = ENV.fetch('RAILS_MAX_THREADS', 3)
 threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch('PORT', 3000)
+# port ENV.fetch('PORT', 3000)
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
@@ -40,3 +40,9 @@ plugin :solid_queue if ENV['SOLID_QUEUE_IN_PUMA']
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
 pidfile ENV['PIDFILE'] if ENV['PIDFILE']
+
+if ENV['PUMA_BIND']
+  bind ENV['PUMA_BIND']
+else
+  port ENV.fetch('PORT', 3000)
+end
